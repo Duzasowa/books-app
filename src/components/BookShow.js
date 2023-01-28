9e3e55e771f9acec02557319a8ba54bfcffa3bbe
Duzasowa <harencko.vitaliy@gmail.com>
2023-01-29 00:46:04 +0100

    Adding, Changing, or Removing Object Properties

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import BookEdit from "./BookEdit";
 
-const BookShow = () => {
-  return <div>BookShow</div>;
+const BookShow = ({ book, onDelete, onEdit }) => {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleDeleteClick = () => {
+    onDelete(book.id);
+  };
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  const handleSumbit = (id, newTitle) => {
+    setShowEdit(false);
+    onEdit(id, newTitle);
+  };
+
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit onSubmit={handleSumbit} book={book} />;
+  }
+
+  return (
+    <div className="book-show">
+      <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/300`} />
+      <div>{content}</div>
+      <div className="actions">
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default BookShow;
